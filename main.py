08a61b0dbe00924993e8a01067cfcf20da772991
AngelBottomless <35677394+aria1th@users.noreply.github.com>
2022-06-24 22:53:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from agent import Agent
 import torch
 from torch import nn
 
+#Example environment
 
-
+'''
+env = gym.make("CartPole-v1")
+observation = env.reset()
+num_actions = env.action_space.n
+'''
 class LayeredModel(nn.Module):
     def __init__(self, in_var = 1, final_var = 4, features = 40, layer_count = 1, p = 0.35, act = nn.ReLU):
         super().__init__()
@@ -22,25 +27,19 @@ class LayeredModel(nn.Module):
     def _layer(self, features):
         return [nn.Linear(features, features), self.act(), nn.Dropout(self.p)]
 
-env = gym.make("CartPole-v1")
-observation = env.reset()
-num_actions = env.action_space.n
 
+#Training and Testing
+    
+'''
 #Example model
 model = LayeredModel(in_var = env.observation_space.shape[0], final_var = env.action_space.n, layer_count = 2)
-
-#Example Agent
+#Agent and environment binding
 agent = Agent(model = model, gamma = 0.99, lr = 3e-4, train = True)
-
-#Bind Environment
 agent.register_env(env)
 
-#Logging
+#Do training
 agent.brain.log_variance = True
-
-#Training
-agent.train_for(480, respect_cycle = True) # train for 480 seconds
-
-#Testing
+agent.train_for(480, respect_cycle = True) # train for 120 seconds
+#Test
 agent.test()
-
+'''
